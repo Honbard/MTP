659eb9d1129ed04b9f42457d10473b5c17a1fb83
Honbard <145313410+Honbard@users.noreply.github.com>
2023-10-24 15:00:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Ngapain inspect? ngebobol kok di source code? Dface kok di gerbang? Nice try kids.
 function transitionToPage2() {
   document.getElementById('page1').classList.add('hidden');
   document.getElementById('page2').classList.remove('hidden');
